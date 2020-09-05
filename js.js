f9222f00ced40477e3dc9a100ec82aeb2ff662a4
thewondersmith <52038146+thewondersmith@users.noreply.github.com>
2020-09-04 21:53:27 -0400

    Add files via upload

--- js.js
+++ js.js
@@ -1,15 +1,5 @@
-//* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
-
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
-    }
-  });
-}
+const toggleButton = document.querySelector('.toggle-menu');
+const navBar = document.querySelector('.nav-bar');
+toggleButton.addEventListener('click', () => {
+  navBar.classList.toggle('toggle');
+});
